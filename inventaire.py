e0cef8753524558105c3182682e291bd2bbc40e1
Émile Jetzer <emile.jetzer@gmail.com>
2022-01-06 11:50:13 -0500

    Chemin absolu sur le disque réseau

--- inventaire.py
+++ inventaire.py
@@ -8,9 +8,38 @@ Created on Tue Jan  4 09:18:43 2022
 @author: emilejetzer
 """
 
-from pathlib import Path
+import platform
 
 from polygphys.inventaire import main
+from pathlib import Path
+
+CHEMINS = {'Darwin': Path('/Volumes/GeniePhysique/Techniciens/'),
+           'Windows': Path(r'Z:'),
+           None: Path('~/.inventaire').expanduser().resolve()}
+SOUS_CHEMIN = Path('Emile_Jetzer/Inventaire/')
 
 if __name__ == '__main__':
-    main(Path('~/.inventaire').expanduser())
+    dossier = CHEMINS.get(platform.system(), CHEMINS[None]) / SOUS_CHEMIN
+
+    if not dossier.exists():
+        dossier.mkdir()
+        cfg = dossier / 'default.cfg'
+        db = dossier / 'inventaire.sqlite'
+
+        with cfg.open('w') as f:
+            f.write(f'''[bd]
+    adresse: sqlite:///{db}
+    tables:
+        boites
+        inventaire
+    formulaires:
+        boites
+        inventaire
+
+[tkinter]
+    title: Inventaire du département de génie physique à Polytechnique Montréal
+''')
+
+        db.touch()
+
+    main(dossier)
